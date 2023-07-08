abaec1b65122ccc67c2f7f579a5641d69864c437
Kirby <012kirby210@gmail.com>
2023-07-08 08:37:57 +0200

    Ajout du chargeur avec affichage de la progression.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,19 +1,19 @@
 import { Suspense, useEffect, useState } from 'react'
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-// import CanvasLoader from '../Loader';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
 
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('./gaming_desktop/scene.gltf');
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={0.12} groundColor="black" />
+      <pointLight intensity={0.75} />
       <spotLight position={[-20,50,10]}
         angle={0.12}
-        intensity={1}
+        intensity={0.5}
         penumbra={1}
         castShadow
         shadow-mapSize={1024}
@@ -21,7 +21,7 @@ const Computers = () => {
       <primitive object={computer.scene}
         scale={0.75}
         position={[0,-3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        rotation={[-0.00, 0.1, -0.15]}
       />
     </mesh>
   )
@@ -34,7 +34,7 @@ const ComputersCanvas = () => {
             camera={{position: [20, 3, 5], fov: 25}}
             gl={{ preserveDrawingBuffer: true}}
     >
-      <Suspense fallback={<></>}>
+      <Suspense fallback={<CanvasLoader />}>
       <OrbitControls enableZoom={false}
                      maxPolarAngle={Math.PI/2}
                      minPolarAngle={Math.PI/2}
